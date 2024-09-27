ddaef58494b6cdeb4437228196abe500ca1050e7
Kowshalyadev <kowshalyagurram@gmail.com>
2024-09-27 10:55:42 +0530

    butterfly

---
+++ ps/butterfly.js
@@ -0,0 +1,34 @@
+rows=+prompt("enter the rows")
+for(i=1;i<=2*rows-1;i++){
+    stars=(i<=rows)?i:2*rows-i
+    space=(i<=rows)?2*rows-(2*i)-1:(2*i-(2*rows)-1)
+    space=(space<0)?0:space
+    res=""
+    for(j=1;j<=stars;j++){
+        if(j%2==0){
+            res+="0 "
+        }
+        else{
+            res+="1 "
+        }
+        
+    }
+    if(i==rows){
+        console.log(res+res.slice(1))
+    }
+    else{
+        console.log(res+"  ".repeat(space)+res.split(" ").reverse().join(" "))
+    }
+}
+
+// output:
+// enter the rows5
+// 1                1
+// 1 0            0 1
+// 1 0 1        1 0 1
+// 1 0 1 0    0 1 0 1
+// 1 0 1 0 1  0 1 0 1 
+// 1 0 1 0    0 1 0 1
+// 1 0 1        1 0 1
+// 1 0            0 1
+// 1                1
---
+++ ps/objtotal.js
@@ -0,0 +1,59 @@
+// Create the function that takes an array with objects and returns the sum of people's budgets.
+
+// Examples :
+
+// getBudgets([
+//   { name: "John", age: 21, budget: 23000 },
+//   { name: "Steve",  age: 32, budget: 40000 },
+//   { name: "Martin",  age: 16, budget: 2700 }
+// ]) ➞ 65700
+
+// getBudgets([
+//   { name: "John",  age: 21, budget: 29000 },
+//   { name: "Steve",  age: 32, budget: 32000 },
+//   { name: "Martin",  age: 16, budget: 1600 }
+// ]) ➞ 62600
+obj=[
+    { name: "John", age: 21, markes: 23000 },
+    { name: "Steve",  age: 32, markes: 40000 },
+    { name: "Martin",  age: 16, markes: 2700 }
+  ]
+  sum=0
+  for(i of obj){
+      sum+=i.markes
+  }
+  console.log(sum)
+
+// output:
+// 65700
+
+// Create a function that takes an array of objects like { name: "John", notes: [3, 5, 4]} and returns an array of objects like { name: "John", avgNote: 4 }. If a student has no notes (an empty array) then let's assume avgNote: 0.
+// 	Example :
+
+// [
+//   { name: "John", notes: [3, 5, 4]}
+// ] ➞ [
+//   { name: "John", avgNote: 4 }
+
+
+obj=[
+    { name: "John", notes: [3, 5, 4]}
+  ] 
+  for(i of obj){
+     sum=0,c=0
+     for(j of i.notes){
+         sum+=j
+         if(j){
+             c=c+1
+         }
+         else{
+            c=1
+         }
+     }
+     i.avgNote=sum/
+     delete i.notes
+  }
+  console.log(obj)
+  
+  
+  
